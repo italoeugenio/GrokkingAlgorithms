ed86349dda35db54cb7a496e4952ff6184e335c2
Ítalo Santana <italoeugenio539@gmail.com>
2024-12-05 15:40:59 -0300

    I need to see where are the problem

--- Algorithm-DataStructure/src/main/SelectionSort/SelectionSort.java
+++ Algorithm-DataStructure/src/main/SelectionSort/SelectionSort.java
@@ -6,19 +6,36 @@ public class SelectionSort {
         int n = list.length - 1;
 
         for(int i = 0; i < n;i++){
-            int min_inx = 1;
+            int min_inx = i;
 
             for(int j = i + 1; j < n; j++){
                 if(list[j] < list[min_inx]){
                     min_inx = j;
                 }
+
             }
             int temp = list[i];
-            list[i] = list[min_idx];
-            list[min_idx] = temp;
+            list[i] = list[min_inx];
+            list[min_inx] = temp;
         }
     }
 
-    public static void main(String[] args) {
+   public static void printArray(int[] arr){
+        for (int val : arr) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
     }
+    public static void main(String[] args) {
+        int[] arr = {5,1,4,5,2};
+
+        System.out.print("Original array: ");
+        printArray(arr);
+
+        selectionSort(arr);
+
+        System.out.print("Sorted array: ");
+        printArray(arr);
+        }
 }
+
