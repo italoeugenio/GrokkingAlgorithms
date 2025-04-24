02380e9f6ce0d1661ab22e86521b97b8c7ca03e3
Ítalo Santana <italoeugenio539@gmail.com>
2025-04-24 16:39:38 -0300

    problema com erro

--- Algorithm-DataStructure/src/main/theprimeage/Asearch/TwoCrystalBallProblem.java
+++ Algorithm-DataStructure/src/main/theprimeage/Asearch/TwoCrystalBallProblem.java
@@ -1,7 +1,29 @@
 package main.theprimeage.Asearch;
 
 public class TwoCrystalBallProblem {
+    public static Integer twoCrystalBalls(boolean[] breaks){
+        int ball = 2;
+        int low = 0;
+        int hight = breaks.length - 1;
+        int mid = 0;
+
+        while(ball > 0){
+            mid = (low + hight)/2;
+            System.out.println(mid);
+            if(breaks[mid]){
+                System.out.println(mid);
+                hight = mid - 1;
+                ball--;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return mid;
+    }
+
     public static void main(String[] args) {
-        boolean[] arr = {true,true,false};
+       boolean[] teste = {false, false, false, false, true, true, true, true, true, true};
+        System.out.println(twoCrystalBalls(teste));
     }
 }
