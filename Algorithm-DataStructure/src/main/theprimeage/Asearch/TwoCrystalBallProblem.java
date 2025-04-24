58164111c64725f8015c3d31f6308e44a7172da0
Ítalo Santana <italoeugenio539@gmail.com>
2025-04-24 17:01:22 -0300

    solutuion to twoCrystalBalls

--- Algorithm-DataStructure/src/main/theprimeage/Asearch/TwoCrystalBallProblem.java
+++ Algorithm-DataStructure/src/main/theprimeage/Asearch/TwoCrystalBallProblem.java
@@ -1,29 +1,28 @@
 package main.theprimeage.Asearch;
 
 public class TwoCrystalBallProblem {
-    public static Integer twoCrystalBalls(boolean[] breaks){
+    public static Integer twoCrystalBalls(boolean[] breaks) {
         int ball = 2;
-        int low = 0;
-        int hight = breaks.length - 1;
-        int mid = 0;
-
-        while(ball > 0){
-            mid = (low + hight)/2;
-            System.out.println(mid);
-            if(breaks[mid]){
-                System.out.println(mid);
-                hight = mid - 1;
+        int index = 0;
+        for (int i = 0; i < breaks.length; i+=2) {
+            if (breaks[i]) {
                 ball--;
-            } else {
-                low = mid + 1;
+                index = i;
+                break;
             }
         }
 
-        return mid;
+        if(!breaks[index - 1]){
+            ball--;
+            return index;
+        } else {
+            ball--;
+            return index - 1;
+        }
     }
 
     public static void main(String[] args) {
-       boolean[] teste = {false, false, false, false, true, true, true, true, true, true};
+        boolean[] teste = {false, false, false, false, true, true, true, true, true, true};
         System.out.println(twoCrystalBalls(teste));
     }
 }
