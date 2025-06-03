3d20b14847ada02a7704f2137e10b87abe0588a4
Ítalo Santana <italoeugenio539@gmail.com>
2025-06-03 11:06:47 -0300

    stack implementation WRONG

---
+++ Algorithm-DataStructure/src/main/theprimeage/Asearch/sort/StackImplementation.java
@@ -0,0 +1,69 @@
+package main.theprimeage.Asearch.sort;
+
+class StackNode {
+    int data;
+    StackNode next;
+
+    public StackNode(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class StackImplementation {
+    private StackNode head;
+    private int length = 0;
+
+    public void push(int data) {
+        StackNode newStack = new StackNode(data);
+        if (head == null) {
+            head = newStack;
+            length++;
+        } else {
+            newStack.next = head;
+            head = newStack;
+            length++;
+        }
+    }
+
+    public void pop() {
+        if (isEmpty()) return;
+        head = head.next;
+        length--;
+    }
+
+    public int peek() {
+        return head.data;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void display() {
+        if (isEmpty()) return;
+        StackNode current = head;
+        while (current.next != null) {
+            System.out.println(current.data);
+            System.out.println("----");
+            current = current.next;
+        }
+        System.out.println("END");
+    }
+
+    public static void main(String[] args) {
+        StackImplementation stack = new StackImplementation();
+        stack.push(5);
+        stack.push(4);
+        stack.push(3);
+        stack.push(2);
+        stack.push(1);
+
+
+
+
+
+        stack.display();
+
+    }
+}
